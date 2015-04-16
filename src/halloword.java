58c622b1244743f8e71bf2f471d170389173831e
thiagooo0 <576713880QQ@qq.com>
2015-04-16 16:36:21 +0800

    4444

---
+++ src/halloword.java
@@ -0,0 +1,12 @@
+
+public class halloword
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("catchcatch");
+		
+		System.out.println("halloword.main()");
+		System.out.println("test4");
+	}
+}
